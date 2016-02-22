a2dbfed6cac47b8dc2bf78fd42b27b4b94892cab
Brian Kemper <bkemper@gmail.com>
2016-02-22 12:18:11 -0700

    look good

--- js/app.js
+++ js/app.js
@@ -2,24 +2,24 @@ angular.module('miniRouting', ['ui.router'])
 
 .config(function($stateProvider, $urlRouterProvider){
 
-$stateProvider
-  .state('home', {
-    url: '/',
-    template: 'js/home/homeTmpl.html',
-    controller: 'homeCtrl'
-  })
-  .state('settings', {
-    url: '/settings',
-    template: 'js/settings/settingsTmpl.html',
-    controller: 'settingsCtrl'
-  })
-  .state('products', {
-    url: '/products/:id',
-    template: 'js/home/productTmpl.html',
-    controller: 'productsCtrl'
-  });
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'js/home/homeTmpl.html',
+      controller: 'homeCtrl'
+    })
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'js/settings/settingsTmpl.html',
+      controller: 'settingsCtrl'
+    })
+    .state('products', {
+      url: '/products/:id',
+      templateUrl: 'js/products/productTmpl.html',
+      controller: 'productsCtrl'
+    });
 
-  $urlRouterProvider
-    .otherwise('/');
+    $urlRouterProvider
+      .otherwise('/');
 
 });
